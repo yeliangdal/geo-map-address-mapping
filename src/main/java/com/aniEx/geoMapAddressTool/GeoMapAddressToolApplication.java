bb87bdca8249b5ffa11c923847a759b6db77d8a7
ye <liangyens2012@gmail.com>
2025-01-08 15:37:02 -0400

    Pushing springboot files

---
+++ src/main/java/com/aniEx/geoMapAddressTool/GeoMapAddressToolApplication.java
@@ -0,0 +1,13 @@
+package com.aniEx.geoMapAddressTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoMapAddressToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoMapAddressToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aniEx/geoMapAddressTool/GeoMapAddressToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aniEx.geoMapAddressTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoMapAddressToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
